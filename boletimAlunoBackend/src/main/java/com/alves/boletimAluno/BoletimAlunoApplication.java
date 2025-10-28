3299bfa9dbef8553bb13ee59bdb29e80746f8480
GuilhermeAlves2596 <guilhermeand21@gmail.com>
2025-10-28 15:51:42 -0300

    Backend criado

---
+++ boletimAlunoBackend/src/main/java/com/alves/boletimAluno/BoletimAlunoApplication.java
@@ -0,0 +1,13 @@
+package com.alves.boletimAluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoletimAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoletimAlunoApplication.class, args);
+	}
+
+}
